4ff211d1f3e78887f4cc369c6e8dbee3792caf2b
Leandro Ramos <leanduardo@hotmail.com>
2022-09-14 11:36:54 -0300

    Interface mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,33 @@
-import { View } from "react-native";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 import { Background } from "./src/components/Background";
 
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
+
 export default function App() {
-  return <Background></Background>;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      ></StatusBar>
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
 }
